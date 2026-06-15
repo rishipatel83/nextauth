fd42ebdd8187ef5a70859a5c09ceffcf05e237a5
Rishi Patel <rishi.777.patel@gmail.com>
2026-06-15 22:31:25 +0530

    Add user retrieval for /me route and token data extraction functionality

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,20 @@
+import { connect } from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from 'next/server'
+import { getDataFromToken } from '@/helpers/getDataFromToken'
+
+connect()
+
+export async function POST(request:NextRequest) {
+    // extract data from token
+    const userId = await getDataFromToken(request)
+    const user = await User.findOne({_id: userId}).select("-password")
+    //check if there is no user
+    if(!user){
+        return NextResponse.json({error: "Invalid token"},{status: 400})
+    }
+    return NextResponse.json({
+        message: "User Found",
+        data: user
+    })
+}
